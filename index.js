8d3e57fcbb336d154059581de7464aba385b64f7
Paul Brown <paulbrown389@gmail.com>
2023-12-14 21:47:01 -0500

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 1;
+let num2 = 62;
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 0
+let num4 = 4
+const mod = num3 + num4
+
+const max = 20
